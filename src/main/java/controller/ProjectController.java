50ccca7fa0daf47af55751ca974a328582ef7292
Paula Belmiro <paulabelmiro@yahoo.com.br>
2022-05-18 10:56:55 -0300

    primeiro commit

---
+++ src/main/java/controller/ProjectController.java
@@ -0,0 +1,147 @@
+
+package controller;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import model.Project;
+import util.ConnectionFactory;
+
+
+public class ProjectController {
+    
+    public void save(Project project){
+        String sql = "INSERT INTO projects (name, "
+               + "description, "
+               + "createdAt, "
+               + "updatedAt) VALUES (?, ?, ?, ?)";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try{
+            //Cria conexão com o banco
+           connection = ConnectionFactory.getConnection();
+           //Cria um PreparedStatement, classe usada para executar a query
+           statement = connection.prepareStatement(sql);
+           
+           statement.setString(1, project.getName());
+           statement.setString(2, project.getDescription());
+           statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+           statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+           
+           //Executa a sql para inserção dos dados
+           statement.execute();
+       } catch(Exception ex){
+           throw new RuntimeException("Erro ao salvar o projeto "
+           + ex.getMessage(), ex);
+       } finally{
+           ConnectionFactory.closeConnection(connection, statement);       
+       }
+    }
+    
+    public void update(Project project){
+        String sql = "UPDATE projects SET "
+               + "name = ?,"
+               + "description = ?, "
+               + "createdAt = ?, "
+               + "updatedAt = ? "
+               + "WHERE id = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try{
+           //Estabelecendo a conexão com o banco de dados
+           connection = ConnectionFactory.getConnection();
+           
+           //Preparando a query
+           statement = connection.prepareStatement(sql);
+           
+           //Setando os valores do statement
+           statement.setString(1, project.getName());
+           statement.setString(2, project.getDescription());
+           statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+           statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+           statement.setInt(5, project.getId());
+           
+           //Executando a query
+           statement.execute();
+       } catch(Exception ex){
+           throw new RuntimeException("Erro ao atualizar o projeto "
+           + ex.getMessage(), ex);
+       } finally{
+           ConnectionFactory.closeConnection(connection, statement);       
+       }
+        
+    }
+    
+    public void removeById(int projectId){
+        String sql = "DELETE FROM projects WHERE id = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        
+        try{
+            //Estabelecendo a conexão com o banco de dados
+           connection = ConnectionFactory.getConnection();
+           
+           //Preparando a query
+           statement = connection.prepareStatement(sql);
+           
+           //Setando os valores
+           statement.setInt(1, projectId);
+           
+           //Executando a query
+           statement.execute();
+       } catch (Exception ex){
+           throw new RuntimeException("Erro ao deletar o projeto " + ex.getMessage(), ex);
+       } finally{
+           ConnectionFactory.closeConnection(connection, statement);
+       }
+    }
+    
+    public List<Project> getAll(){
+        String sql = "SELECT * FROM projects";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        
+        List<Project> projects = new ArrayList<Project>();
+            
+    try{
+           //Estabelecendo a conexão com o banco de dados
+           connection = ConnectionFactory.getConnection();
+           
+           //Preparando a query
+           statement = connection.prepareStatement(sql);
+                   
+           //Valor retornado pela execução da query
+           resultSet = statement.executeQuery();
+           
+           //Enquanto houverem valores a serem percorridos no meu resultSet
+           while(resultSet.next()){
+               
+               Project project = new Project();
+               
+               project.setId(resultSet.getInt("id"));
+               project.setName(resultSet.getString("name"));
+               project.setDescription(resultSet.getString("description"));
+               project.setCreatedAt(resultSet.getDate("createdAt"));
+               project.setUpdatedAt(resultSet.getDate("UpdatedAt"));
+               projects.add(project);
+           }
+        } catch(Exception ex){
+           throw new RuntimeException("Erro ao gerar a lista de projetos "
+           + ex.getMessage(), ex);
+        } finally{
+           ConnectionFactory.closeConnection(connection, statement, resultSet);       
+       }
+        //Lista de tarefas que foi criada e carregada do banco de dados 
+        return projects;
+    }
+}
---
+++ src/main/java/temTask/Main.java
@@ -0,0 +1,20 @@
+
+package temTask;
+
+import controller.ProjectController;
+import java.util.List;
+import model.Project;
+
+/**
+ *
+ * @author Paula Belmiro
+ */
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        
+       
+    }
+    
+}
